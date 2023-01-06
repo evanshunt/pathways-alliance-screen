297ffd5b50c6ed1c0afd7445ea1008e0ff0263e1
dmcb <d.mcburney@icloud.com>
2023-01-06 15:55:47 -0700

    Experiments with font

--- src/js/Experience.jsx
+++ src/js/Experience.jsx
@@ -1,7 +1,8 @@
 import { useThree, extend, useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
-import { Text, Html, Effects } from '@react-three/drei'
+import { MeshReflectorMaterial, Float, Text, Html, Effects } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
+import { Mesh } from 'three';
 
 extend({ UnrealBloomPass });
 
@@ -9,8 +10,8 @@ export default function Experience() {
   const textRef = useRef();
 
   useFrame((state, delta) => {
-    textRef.current.rotation.z = Math.sin(state.clock.elapsedTime) * 0.04;
-    textRef.current.rotation.y = Math.cos(state.clock.elapsedTime) * 0.04;
+    // textRef.current.rotation.z = Math.sin(state.clock.elapsedTime) * 0.04;
+    textRef.current.rotation.y = Math.cos(state.clock.elapsedTime * 0.5) * -4 * (Math.PI/180) - (15 * Math.PI/180);
   });
 
   return <>
@@ -22,15 +23,32 @@ export default function Experience() {
     
     <pointLight position={[0,0,40]} intensity={0.5} />
 
-    <group ref={textRef}>
-      <Text fontSize={0.5} position={[0,0.25,0]}>
-        There is no single path to
-        <meshStandardMaterial />
-      </Text>
-      <Text fontSize={0.5} position={[0,-0.25,0]}>
-        net zero.
-        <meshStandardMaterial />
-      </Text>
-    </group>
+    <Text 
+      ref={textRef} 
+      position={[-0.75,0.75,0]} 
+      textAlign="center" 
+      maxWidth={10}
+      letterSpacing={-0.08}
+      lineHeight={0.8}
+    >
+      There is no single path to net zero.
+      <meshStandardMaterial />
+    </Text>
+
+    <mesh position={[0,-0.1,5]} rotation-x={-Math.PI*0.5}>
+      <planeGeometry args={[50,50]} />
+      <MeshReflectorMaterial 
+        resolution={2048}
+        blur={[500,500]}
+        mixBlur={0.8}
+        mirror={1}
+        mixStrength={30}
+        minDepthThreshold={1}
+        maxDepthThreshold={4}
+        roughness={1}
+        depthScale={0.2}
+        metalness={0.5}
+      />
+    </mesh>
   </>
 }
