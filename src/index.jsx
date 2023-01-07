77748afc1e7b0d2ed666169236720d010eabff2f
dmcb <d.mcburney@icloud.com>
2023-01-07 09:44:08 -0700

    Adds debug and perf

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import Experience from './js/Experience.jsx';
+import { Leva } from 'leva';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
+    <Leva collapsed />
     <Canvas>
       <Experience />
     </Canvas>
-  </React.StrictMode>,
+  </StrictMode>,
 );
--- src/js/Experience.jsx
+++ src/js/Experience.jsx
@@ -3,22 +3,45 @@ import { useRef } from 'react';
 import { MeshReflectorMaterial, Float, Text, Html, Effects } from '@react-three/drei'
 import { UnrealBloomPass } from 'three-stdlib'
 import { Mesh } from 'three';
+import { useControls } from 'leva';
+import { Perf } from 'r3f-perf';
 
 extend({ UnrealBloomPass });
 
 export default function Experience() {
   const textRef = useRef();
 
+  const {perfVisible} = useControls({
+    perfVisible: false
+  });
+
+  const {degreesOfRotation, rotationSpeed} = useControls('Text', {
+    degreesOfRotation: {
+      value: 6,
+      min: 0,
+      max: 45,
+      step: 1
+    },
+    rotationSpeed: {
+      value: 0.5,
+      min: 0,
+      max: 2,
+      step: 0.1
+    }
+  });
+
   useFrame((state, delta) => {
     // textRef.current.rotation.z = Math.sin(state.clock.elapsedTime) * 0.04;
-    textRef.current.rotation.y = Math.cos(state.clock.elapsedTime * 0.5) * -4 * (Math.PI/180) - (15 * Math.PI/180);
+    textRef.current.rotation.y = Math.cos(state.clock.elapsedTime * rotationSpeed) * -degreesOfRotation * (Math.PI/180) - (12 * Math.PI/180);
   });
 
   return <>
+    { perfVisible && <Perf position="top-left" />}
+
     <ambientLight intensity={ 0.03 }/>
 
     <Effects disableGamma>
-      <unrealBloomPass threshold={0.5} strength={0.5} radius={1} />
+      <unrealBloomPass threshold={0.5} strength={0.7} radius={1} />
     </Effects>
     
     <pointLight position={[0,0,40]} intensity={0.5} />
