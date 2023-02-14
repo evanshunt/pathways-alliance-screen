82c0f373014c38c6ac89868dd9030e8596969e4c
Justin Luk <justin.luk@gmail.com>
2023-02-14 12:50:22 -0500

    Animating circle for Together slide

--- src/js/Components/Slides/SlideTogether.jsx
+++ src/js/Components/Slides/SlideTogether.jsx
@@ -1,75 +1,136 @@
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import { DrawSVGPlugin } from "gsap/all";
+
 import AnimNumberAccumulator from "../Animations/AnimNumberAccumulator";
 import AnimSlideUpFadeIn from "../Animations/AnimSlideUpFadeIn";
 
+gsap.registerPlugin(DrawSVGPlugin);
+
 export default ({ t, isActive }) => {
+  const circleRef = useRef();
+  const timeline = useRef();
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.set(circleRef.current, {
+        drawSVG: 0,
+        rotation: -90,
+        transformOrigin: "center center",
+      });
+
+      timeline.current = gsap.timeline().to(circleRef.current, {
+        drawSVG: "0% 100%",
+        delay: 0.8 + 0.5,
+        duration: 1,
+      });
+    }, circleRef);
+
+    return () => ctx.revert();
+  }, []);
+
+  useLayoutEffect(() => {
+    if (isActive) {
+      timeline.current.play(0);
+    }
+  }, [isActive]);
+
   return (
     <div id="slide-together">
       <div className="highlight">
-        <h1>
-          <span className="text">
-            {t("slides.together.highlightBeforeNumberText")}
-          </span>
-          <span className="number">
-            <AnimNumberAccumulator
-              number={95}
-              isActive={isActive}
-              toFixed={0}
-            />
-            %
-          </span>{" "}
-          <span className="text">
-            {t("slides.together.highlightAfterNumberText")}
-          </span>
-        </h1>
+        <div className="background">
+          <svg width="555" height="555" preserveAspectRatio="xMidYMid meet">
+            <mask id="svg-mask">
+              <circle
+                stroke="#ffffff"
+                cx="280"
+                cy="280"
+                r="150"
+                ref={circleRef}
+              ></circle>
+            </mask>
+            <image
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              xlinkHref="/images/slides/together-highlight-bg.png"
+              mask="url(#svg-mask)"
+              x="0"
+              y="0"
+              width="100%"
+              height="100%"
+            ></image>
+          </svg>
+        </div>
+        <AnimSlideUpFadeIn isActive={isActive}>
+          <div className="heading">
+            <h1>
+              <span className="text">
+                {t("slides.together.highlightBeforeNumberText")}
+              </span>
+              <span className="number">
+                <AnimNumberAccumulator
+                  number={95}
+                  isActive={isActive}
+                  toFixed={0}
+                  delay={0.5}
+                />
+                %
+              </span>{" "}
+              <span className="text">
+                {t("slides.together.highlightAfterNumberText")}
+              </span>
+            </h1>
+          </div>
+        </AnimSlideUpFadeIn>
       </div>
       <div className="stats">
         <div className="bucket">
-          <AnimSlideUpFadeIn isActive={isActive}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={0.5}>
             <h2>
               $
               <AnimNumberAccumulator
                 number={16.5}
                 isActive={isActive}
                 toFixed={1}
+                delay={0.7}
               />{" "}
               {t("slides.together.bucket1Heading")}
             </h2>
           </AnimSlideUpFadeIn>
-          <AnimSlideUpFadeIn isActive={isActive} delay={0.2}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={0.7}>
             <p>{t("slides.together.bucket1Text")}</p>
           </AnimSlideUpFadeIn>
         </div>
 
         <div className="bucket">
-          <AnimSlideUpFadeIn isActive={isActive} delay={0.4}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={0.9}>
             <h2>
               $
               <AnimNumberAccumulator
                 number={7.6}
                 isActive={isActive}
                 toFixed={1}
-                delay={0.4}
+                delay={1.1}
               />{" "}
               {t("slides.together.bucket2Heading")}
             </h2>
           </AnimSlideUpFadeIn>
-          <AnimSlideUpFadeIn isActive={isActive} delay={0.6}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={1.1}>
             <p>{t("slides.together.bucket2Text")}</p>
           </AnimSlideUpFadeIn>
         </div>
         <div className="bucket reverse">
-          <AnimSlideUpFadeIn isActive={isActive} delay={1}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={1.3}>
             <h2>
               <AnimNumberAccumulator
                 number={35000}
                 isActive={isActive}
                 toFixed={0}
-                delay={1}
+                delay={1.5}
               />{" "}
               {t("slides.together.bucket3Heading")}
             </h2>
           </AnimSlideUpFadeIn>
-          <AnimSlideUpFadeIn isActive={isActive} delay={0.8}>
+          <AnimSlideUpFadeIn isActive={isActive} delay={1.5}>
             <p>{t("slides.together.bucket3Text")}</p>
           </AnimSlideUpFadeIn>
         </div>
