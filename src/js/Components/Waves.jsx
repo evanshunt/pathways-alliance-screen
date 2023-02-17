1ccc425dda60293e6bfa3543471df59a01b8e863
dmcb <d.mcburney@icloud.com>
2023-02-17 08:38:05 -0700

    Adds more wave variables

--- src/js/Components/Waves.jsx
+++ src/js/Components/Waves.jsx
@@ -10,6 +10,8 @@ const Waves = ({ maxSceneLength }) => {
 
   const [
     {
+      lineWidth,
+      opacity,
       numWaves,
       amplitude,
       frequency,
@@ -24,13 +26,15 @@ const Waves = ({ maxSceneLength }) => {
     },
     set,
   ] = useControls("Waves", () => ({
+    lineWidth: { value: 0.02, min: 0.01, max: 0.1, step: 0.01 },
+    opacity: { value: 1, min: 0, max: 1, step: 0.01 },
     numWaves: { value: 20, min: 1, max: 40, step: 1 },
     amplitude: { value: 3.5, min: 0, max: 10, step: 0.1 },
     frequency: { value: 4.5, min: 0, max: 10, step: 0.1 },
     numPoints: { value: 215, min: 10, max: 400, step: 1 },
     animationSpeed: { value: 0.2, min: 0, max: 1, step: 0.01 },
-    lineOffset: { value: 0.05, min: 0, max: 1, step: 0.01 },
-    pointOffset: { value: 0.05, min: 0, max: 1, step: 0.01 },
+    lineOffset: { value: 0.05, min: -0.3, max: 0.3, step: 0.01 },
+    pointOffset: { value: 0.05, min: -0.3, max: 0.3, step: 0.01 },
     xOffsetMultiplier: { value: 0.6, min: 0, max: 2, step: 0.01 },
     yOffset: { value: 0, min: -20, max: 20, step: 0.1 },
     yOffsetMultiplier:  { value: 0.13, min: 0, max: 1, step: 0.01 },
@@ -60,7 +64,9 @@ const Waves = ({ maxSceneLength }) => {
   const meshLineMaterial = (
     <meshLineMaterial
       attach="material"
-      lineWidth={0.02}
+      lineWidth={lineWidth}
+      transparent={true}
+      opacity={opacity}
       color={[0, 238, 250]} // #00eefa
     />
   );
