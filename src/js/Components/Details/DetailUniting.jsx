e4f233d15fcd077b6fff35b3f4c7bf23430dc8e3
Justin Luk <justin.luk@gmail.com>
2023-02-21 15:05:09 -0500

    Animating text into first detail view

--- src/js/Components/Details/DetailUniting.jsx
+++ src/js/Components/Details/DetailUniting.jsx
@@ -1,33 +1,53 @@
+import AnimSlideUpFadeIn from "../Animations/AnimSlideUpFadeIn";
+
 export default ({ t }) => {
   return (
     <div id="detail-uniting">
       <div className="text">
-        <h2>{t("details.uniting.heading")}</h2>
-        {t("details.uniting.text")
-          .split("\n")
-          .map((paragraph, i) => (
-            <p key={`detail-uniting-paragraph-${i}`}>{paragraph}</p>
-          ))}
+        <AnimSlideUpFadeIn>
+          <h2>{t("details.uniting.heading")}</h2>
+        </AnimSlideUpFadeIn>
+        <AnimSlideUpFadeIn delay={0.2}>
+          <div className="paragraph-wrapper">
+            {t("details.uniting.text")
+              .split("\n")
+              .map((paragraph, i) => (
+                <p key={`detail-uniting-paragraph-${i}`}>{paragraph}</p>
+              ))}
+          </div>
+        </AnimSlideUpFadeIn>
       </div>
-      <div className="illustration">
-        <ul className="logos">
+      <div className="logos">
+        <ul>
           <li>
-            <img src="/images/logos/canadian-natural.png" />
+            <AnimSlideUpFadeIn delay={0.4}>
+              <img src="/images/logos/canadian-natural.png" />
+            </AnimSlideUpFadeIn>
           </li>
           <li>
-            <img src="/images/logos/cenovus.png" />
+            <AnimSlideUpFadeIn delay={0.5}>
+              <img src="/images/logos/cenovus.png" />
+            </AnimSlideUpFadeIn>
           </li>
           <li>
-            <img src="/images/logos/conoco-phillips.png" />
+            <AnimSlideUpFadeIn delay={0.6}>
+              <img src="/images/logos/conoco-phillips.png" />
+            </AnimSlideUpFadeIn>
           </li>
           <li>
-            <img src="/images/logos/imperial.png" />
+            <AnimSlideUpFadeIn delay={0.7}>
+              <img src="/images/logos/imperial.png" />
+            </AnimSlideUpFadeIn>
           </li>
           <li>
-            <img src="/images/logos/meg.png" />
+            <AnimSlideUpFadeIn delay={0.8}>
+              <img src="/images/logos/meg.png" />
+            </AnimSlideUpFadeIn>
           </li>
           <li>
-            <img src="/images/logos/suncor.png" />
+            <AnimSlideUpFadeIn delay={0.9}>
+              <img src="/images/logos/suncor.png" />
+            </AnimSlideUpFadeIn>
           </li>
         </ul>
       </div>
