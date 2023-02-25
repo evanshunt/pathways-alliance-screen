fb09791ef7c7077db898b8b632494fd57a0d94f7
dmcb <d.mcburney@icloud.com>
2023-02-24 17:34:37 -0700

    Fixes bubbles again maybe ugh

--- src/js/Components/Bubble.jsx
+++ src/js/Components/Bubble.jsx
@@ -51,7 +51,7 @@ export default forwardRef(function (
       the HTML button itself doesn't work because the drag control can
       no longer stop its activation. Turn on visibility to see how
       crummy this is. In real life usage though it should be decent. */}
-      <Circle args={[2.2, 64]} position={index % 2 ? [13.8,1.6,0] : [13.8,-1.3,0]} onPointerUp={active && handlePointerUp}>
+      <Circle args={[2.2, 64]} position={index % 2 ? [12.1,1.6,0] : [12.1,-1.3,0]} onPointerUp={active && handlePointerUp}>
         <meshStandardMaterial visible={false} />
       </Circle>
       <Html
